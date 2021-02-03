231b3f955d296b23a0e2c3d6f1df6c433d0ceab2
roidefrance <kirmaxmel@gmail.com>
2021-02-03 21:59:14 +0300

    revision

--- src/test/java/tests/demoqaTests.java
+++ src/test/java/tests/demoqaTests.java
@@ -18,17 +18,17 @@ class demoqaTests {
 @BeforeAll
 static void setup(){
     Configuration.startMaximized = true;
-    Configuration.timeout = 60000;
+    Configuration.timeout = 6000;
 }
     @Test
     void siteExists() {
 
         open("https://demoqa.com/automation-practice-form");
-        $("#firstName").setValue("Kirill").shouldBe(visible);
-        $("#lastName").setValue("Melnikov");
-        $("#userEmail").setValue("xyz@aaa.net");
+        $("#firstName").val("Kirill").shouldBe(visible);
+        $("#lastName").val("Melnikov");
+        $("#userEmail").val("xyz@aaa.net");
         $(byText("Male")).click();
-        $("#userNumber").setValue("9515082525");
+        $("#userNumber").val("9515082525");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("September");
         $(".react-datepicker__year-select").selectOption("1985");
@@ -37,7 +37,7 @@ static void setup(){
         $("#subjectsInput").val("Arts").pressEnter();
         $(byText("Music")).click();
         $("#uploadPicture").uploadFile(new File("src/test/java/tests/resources/1.txt"));
-        $("#currentAddress").setValue("elm st");
+        $("#currentAddress").val("elm st");
         $("#state").scrollTo();
         $("#state").click();
         $(byText("NCR")).click();
@@ -45,6 +45,8 @@ static void setup(){
         $(byText("Delhi")).click();
         $("#submit").click();
         $(".modal-body").shouldHave(text("Thanks for submitting the form"), text("Kirill"));
+        $("#closeLargeModal").scrollTo();
+        $("#closeLargeModal").click();
 
     }
 
