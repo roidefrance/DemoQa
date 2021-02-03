74fcd3403862c6ad2f55dad00d4fd97e9aba68d8
roidefrance <kirmaxmel@gmail.com>
2021-02-03 21:44:19 +0300

    revision

--- src/test/java/tests/demoqaTests.java
+++ src/test/java/tests/demoqaTests.java
@@ -5,27 +5,26 @@ import com.codeborne.selenide.commands.ScrollTo;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.text;
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byClassName;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 class demoqaTests {
 
 @BeforeAll
 static void setup(){
     Configuration.startMaximized = true;
+    Configuration.timeout = 60000;
 }
     @Test
     void siteExists() {
 
-        open("https://demoqa.com/");
-        $("html").shouldHave(text("Forms"));
-        $(byText("Forms")).scrollTo();
-        $(byText("Forms")).click();
-        $(byText("Practice Form")).click();
-        $("#firstName").setValue("Kirill");
+        open("https://demoqa.com/automation-practice-form");
+        $("#firstName").setValue("Kirill").shouldBe(visible);
         $("#lastName").setValue("Melnikov");
         $("#userEmail").setValue("xyz@aaa.net");
         $(byText("Male")).click();
@@ -34,11 +33,10 @@ static void setup(){
         $(".react-datepicker__month-select").selectOption("September");
         $(".react-datepicker__year-select").selectOption("1985");
         $(".react-datepicker__day--030").click();
-        $("#subjectsInput").val("M");
-        $(".subjects-auto-complete__menu-list").$(byText("Maths")).click();
-        $("#subjectsInput").val("A");
-        $(".subjects-auto-complete__menu-list").$(byText("Arts")).click();
+        $("#subjectsInput").val("Math").pressEnter();
+        $("#subjectsInput").val("Arts").pressEnter();
         $(byText("Music")).click();
+        $("#uploadPicture").uploadFile(new File("src/test/java/tests/resources/1.txt"));
         $("#currentAddress").setValue("elm st");
         $("#state").scrollTo();
         $("#state").click();
@@ -46,8 +44,7 @@ static void setup(){
         $("#city").click();
         $(byText("Delhi")).click();
         $("#submit").click();
-        $("html").shouldHave(text("Thanks for submitting the form"));
-
+        $(".modal-body").shouldHave(text("Thanks for submitting the form"), text("Kirill"));
 
     }
 
